2c112c4094a2bb43257259341aa8126a0d283591
igor <suporteaser2@gmail.com>
2025-02-01 22:18:02 -0300

    finalizado a screen profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,11 +1,75 @@
+import { useState } from "react";
+
 import { ScrollView, TouchableOpacity } from "react-native";
-import { Center, Heading, Text, VStack } from "@gluestack-ui/themed";
+import { Center, Heading, Text, VStack, useToast } from "@gluestack-ui/themed";
+
+import * as FileSystem from "expo-file-system";
+import * as ImagePicker from "expo-image-picker";
+
 import { ScreenHeader } from "@/components/ScreenHeader";
 import { UserPhoto } from "@/components/UserPhoto";
 import { Input } from "@/components/Input";
 import { Button } from "@/components/Button";
+import { ToastMessage } from "@/components/ToastMessage";
 
 export function Profile() {
+  const [userPhoto, setUserPhoto] = useState("https://github.com/LElTEDEV.png");
+  const toast = useToast();
+
+  async function handleUserPhotoSelect() {
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: "images",
+        allowsEditing: true,
+        aspect: [4, 4],
+        quality: 1,
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      const photoURI = photoSelected.assets[0].uri;
+
+      if (photoURI) {
+        const photoInfo = (await FileSystem.getInfoAsync(photoURI)) as {
+          size: number;
+        };
+
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
+          return toast.show({
+            placement: "top",
+            render: ({ id }) => (
+              <ToastMessage
+                id={id}
+                action="error"
+                title="Arquivo acima do tamanho permitido!"
+                description="Por favor, selecione um arquivo de no máximo 5MB"
+                onClose={() => toast.close(id)}
+              />
+            ),
+          });
+        }
+
+        toast.show({
+          placement: "top",
+          render: ({ id }) => (
+            <ToastMessage
+              id={id}
+              title="Foto carregada com sucesso!"
+              action="success"
+              onClose={() => toast.close(id)}
+            />
+          ),
+        });
+
+        setUserPhoto(photoURI);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
@@ -13,12 +77,12 @@ export function Profile() {
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt="$6" px="$10">
           <UserPhoto
-            source={{ uri: "https://github.com/LElTEDEV.png" }}
+            source={{ uri: userPhoto }}
             alt="Foto do usuário"
             size="xl"
           />
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               color="$green500"
               fontFamily="$heading"
